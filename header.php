974a23c7b58b400e4390bf56cc2fd72f78b0fd2b
manudevappas <50258901+manudevappas@users.noreply.github.com>
2020-07-17 18:06:58 +0530

    initial

---
+++ header.php
@@ -0,0 +1,13 @@
+
+  <!-- <div class="container" -->
+    <header class="header-basic-light">
+        <div class="header-limiter">
+            <h1><a href="../index.php">Market <span>Meter</span></a></h1>
+            <nav>
+                <a href="/price/index.php">Market</a>
+                <a href="/price/compare/index.php">Compare market</a>
+                <a href="/price/chart/index.php">Graph</a>               
+                <a href="/price/admin/upload.php">Account</a>
+            </nav>
+        </div>
+    </header>
